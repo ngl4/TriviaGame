d406c8f31b319daa746d5bf1396ad059c9215cfe
Binh Lam <cindybinh444@gmail.com>
2018-08-29 17:12:04 -0400

    add pseudo-code

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+//START GAME
+//Start Button 
+
+    //once the start button is clicked:
+        //the start button disappears
+        //the forms appear
+        //the timer starts counting down from 30 seconds
+        //*** Appear and Disapper can be done using .style.display = "none";
+
+//Forms
+
+    //all selected input in stored within variables 
+    //define the correct answers for each questions through variables 
+    //compare the selected input with the correct answers
+    //if they match, then add 1 to the correct answer 
+    //if they are not match, add 1 to the incorrect answer 
+    //if they are not answered, add 1 to the no answer 
+
+//END GAME
+
+//Timers get to ZERO 
+
+    //the forms disappear 
+    //the final answer page appears 
+
+//Submit Button 
+    //once the submit button is clicked: 
+        //the forms disappear 
+        //the final answer page appears 
+
+
+//Final Answer
+
+    //Display all the answers in HTML page
+    //Create a "RESTART" button to allow users to replay the game 
