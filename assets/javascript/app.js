ed733a93e15ac697a70a5a1526c40016fa8ff9d0
Binh Lam <cindybinh444@gmail.com>
2018-08-30 16:07:45 -0400

    add restart button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,12 @@ var gameStarted = false;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var noAnswer = 0;
+var submitClick = false;
 
 //  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
 var timerStart = false;
-var time = 10;
+var time = 5;
 var decrementTime;
 
 //Game started once HTML is completely loaded
@@ -26,42 +27,8 @@ $(document).ready(function() {
 
     //Submit button is clicked
     $("#submit").click(function() {
-      $("#form-questions").hide();
-      $("#final-answer").show();
-
-      var answer1 = $("input[type=radio][name=joke]:checked").val();
-      if (answer1 === "bed") {
-        correctAnswers++;
-      } else if (answer1 === "clock" || answer1 === "snake") {
-        incorrectAnswers++;
-      } else {
-        noAnswer++;
-      }
-
-      var answer2 = $("input[type=radio][name=joke2]:checked").val();
-      if (answer2 === "snake") {
-        correctAnswers++;
-      } else if (answer2 === "clock" || answer2 === "bed") {
-        incorrectAnswers++;
-      } else {
-        noAnswer++;
-      }
-
-      var answer3 = $("input[type=checkbox][name=joke3]:checked").val();
-      if (answer3 === "bed" && answer3 === "snake") {
-        correctAnswers++;
-      } else if (
-        answer3 === "clock" ||
-        answer3 === "bed" ||
-        answer3 === "snake"
-      ) {
-        incorrectAnswers++;
-      } else {
-        noAnswer++;
-      }
-      $("#correct-answer").text(correctAnswers);
-      $("#incorrect-answer").text(incorrectAnswers);
-      $("#no-answer").text(noAnswer);
+      submitClick = true;
+      displayAnswers();
     });
   });
 });
@@ -70,7 +37,7 @@ function startGame() {
   gameStarted = true;
   $("#start-button").show();
   $("#form-questions").hide();
-  $("#final-answer").hide();
+  $("#final-answers").hide();
 }
 
 function decrement() {
@@ -80,42 +47,9 @@ function decrement() {
   if (time === -1) {
     stop();
     console.log("Time's Up");
-    $("#form-questions").hide();
-    $("#final-answer").show();
-
-    var answer1 = $("input[type=radio][name=joke]:checked").val();
-    if (answer1 === "bed") {
-      correctAnswers++;
-    } else if (answer1 === "clock" || answer1 === "snake") {
-      incorrectAnswers++;
-    } else {
-      noAnswer++;
+    if (submitClick) {
+      displayAnswers(); //TODO: Display answers only if the submit button is not clicked!
     }
-
-    var answer2 = $("input[type=radio][name=joke2]:checked").val();
-    if (answer2 === "snake") {
-      correctAnswers++;
-    } else if (answer2 === "clock" || answer2 === "bed") {
-      incorrectAnswers++;
-    } else {
-      noAnswer++;
-    }
-
-    var answer3 = $("input[type=checkbox][name=joke3]:checked").val();
-    if (answer3 === "bed" && answer3 === "snake") {
-      correctAnswers++;
-    } else if (
-      answer3 === "clock" ||
-      answer3 === "bed" ||
-      answer3 === "snake"
-    ) {
-      incorrectAnswers++;
-    } else {
-      noAnswer++;
-    }
-    $("#correct-answer").text(correctAnswers);
-    $("#incorrect-answer").text(incorrectAnswers);
-    $("#no-answer").text(noAnswer);
   }
 }
 
@@ -128,7 +62,49 @@ function stop() {
   clearInterval(intervalId);
 }
 
+function displayAnswers() {
+  $("#form-questions").hide();
+  $("#final-answers").show();
+
+  var answer1 = $("input[type=radio][name=joke]:checked").val();
+  if (answer1 === "bed") {
+    correctAnswers++;
+  } else if (answer1 === "clock" || answer1 === "snake") {
+    incorrectAnswers++;
+  } else {
+    noAnswer++;
+  }
+
+  var answer2 = $("input[type=radio][name=joke2]:checked").val();
+  if (answer2 === "snake") {
+    correctAnswers++;
+  } else if (answer2 === "clock" || answer2 === "bed") {
+    incorrectAnswers++;
+  } else {
+    noAnswer++;
+  }
+
+  var answer3 = $("input[type=checkbox][name=joke3]:checked").val();
+  if (answer3 === "bed" && answer3 === "snake") {
+    correctAnswers++; //TODO: Check this logic again!!!
+    console.log(answer3);
+  } else if (answer3 === "clock" || answer3 === "bed" || answer3 === "snake") {
+    incorrectAnswers++;
+  } else {
+    noAnswer++;
+  }
+  $("#correct-answer").text(correctAnswers);
+  $("#incorrect-answer").text(incorrectAnswers);
+  $("#no-answer").text(noAnswer);
+}
+
+//Restart Button 
+var restartButton = $("<button>");
+restartButton.text("RESTART");
+$("#final-answers").append(restartButton);
+restartButton.click(function() {
+  location.reload();
+});
 
 
-//Create a "RESTART" button to allow users to replay the game
-//Shorten code using function for repetitive code 
+//Fix the TODOs logic problem!!! 
