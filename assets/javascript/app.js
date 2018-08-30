8fc07d0136fbd891505efc108c3918ee4e552b6c
Binh Lam <cindybinh444@gmail.com>
2018-08-30 14:37:51 -0400

    clear out already done pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
 //Declare Variables
 var gameStarted = false;
 var correctAnswers = 0;
-var incorrectAnswers =0;
+var incorrectAnswers = 0;
 var noAnswer = 0;
 
 //  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
 var timerStart = false;
-var time = 30;
+var time = 10;
+var decrementTime;
 
 //Game started once HTML is completely loaded
 $(document).ready(function() {
@@ -19,32 +20,48 @@ $(document).ready(function() {
     $("#form-questions").show();
 
     if (!timerStart) {
-        console.log("Timer has started!");
-        start();
+      console.log("Timer has started!");
+      start();  
     }
 
     //Submit button is clicked
     $("#submit").click(function() {
       $("#form-questions").hide();
       $("#final-answer").show();
-      
+
       var answer1 = $("input[type=radio][name=joke]:checked").val();
       if (answer1 === "bed") {
-
         correctAnswers++;
-        console.log(correctAnswers);
-
-      }else if (answer1 === "clock" || answer1 === "snake" ){
-          
+      } else if (answer1 === "clock" || answer1 === "snake") {
         incorrectAnswers++;
-        console.log(incorrectAnswers);
-        
-      }else {
-
+      } else {
         noAnswer++;
-        console.log(noAnswer);
       }
-      
+
+      var answer2 = $("input[type=radio][name=joke2]:checked").val();
+      if (answer2 === "snake") {
+        correctAnswers++;
+      } else if (answer2 === "clock" || answer2 === "bed") {
+        incorrectAnswers++;
+      } else {
+        noAnswer++;
+      }
+
+      var answer3 = $("input[type=checkbox][name=joke3]:checked").val();
+      if (answer3 === "bed" && answer3 === "snake") {
+        correctAnswers++;
+      } else if (
+        answer3 === "clock" ||
+        answer3 === "bed" ||
+        answer3 === "snake"
+      ) {
+        incorrectAnswers++;
+      } else {
+        noAnswer++;
+      }
+      $("#correct-answer").text(correctAnswers);
+      $("#incorrect-answer").text(incorrectAnswers);
+      $("#no-answer").text(noAnswer);
     });
   });
 });
@@ -57,46 +74,26 @@ function startGame() {
 }
 
 function count() {
-    var decrementTime = time--;
-    $("#timers").text(decrementTime);
+  decrementTime = time--;
+  $("#timers").text(decrementTime);
+  
 }
 
 //Use setInterval to start the count here
 function start() {
-    intervalId = setInterval(count, 1000);
+  intervalId = setInterval(count, 1000);
 }
 
-//START GAME
-//Start Button
+function stop() {
+  clearInterval(intervalId);
+}
 
-//once the start button is clicked:
-//the start button disappears
-//the forms appear
-//the timer starts counting down from 30 seconds
-//*** Appear and Disapper can be done using .hide() and .show() using JQuery;
-
-//Forms
-
-//all selected input in stored within variables
-//define the correct answers for each questions through variables
-//compare the selected input with the correct answers
-//if they match, then add 1 to the correct answer
-//if they are not match, add 1 to the incorrect answer
-//if they are not answered, add 1 to the no answer
 
 //END GAME
 
 //Timers get to ZERO
-
 //the forms disappear
 //the final answer page appears
 
-//Submit Button
-//once the submit button is clicked:
-//the forms disappear
-//the final answer page appears
 
-//Final Answer
-
-//Display all the answers in HTML page
 //Create a "RESTART" button to allow users to replay the game
